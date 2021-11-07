92e600f6bd9bb94c941281aea6f08a9df1181cbd
Desarrollador Web <desarrolloweb@seguridadpenta.com>
2021-11-07 13:12:53 -0500

    Added list of comment to post

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -8,6 +8,7 @@ import Modal from '../Modal';
 import axiosInstance from '../../axiosConfig';
 import Loader from '../Loader';
 import UserCard from '../UserCard';
+import CommentCard from '../CommentCard.js';
 
 const { useState } = React;
 
@@ -29,9 +30,20 @@ const PostCard = ({
     data: null
   });
 
+  const [commentsModalOptions, setCommentsModalOptions] = useState({
+    show: false,
+    loading: false,
+    error: null,
+    comments: []
+  });
+
   const handleOpenUserModal = () => setUserModalOptions(prevState => ({ ...prevState, show: true }));
 
   const handleCloseUserModal = () => setUserModalOptions({ show: false, loading: false, error: null, data: null });
+
+  const handleOpenCommentsModal = () => setCommentsModalOptions(prevState => ({ ...prevState, show: true }));
+
+  const handleCloseCommentsModal = () => setCommentsModalOptions({ show: false, loading: false, error: null, comments: [] });
 
   const getUserInfo = async () => {
 
@@ -57,6 +69,34 @@ const PostCard = ({
 
     } finally {
       setUserModalOptions(prevState => ({ ...prevState, loading: false }));
+    }
+
+  };
+
+  const getPostComments = async () => {
+
+    setCommentsModalOptions(prevState => ({ ...prevState, loading: true }));
+
+    try {
+
+      const axiosResponse = await axiosInstance(`post/${id}/comment`);
+      const { data } = axiosResponse.data;
+
+      setCommentsModalOptions(prevState => ({
+        ...prevState,
+        comments: data
+      }));
+
+    } catch (error) {
+
+      if (error?.error) {
+        setCommentsModalOptions(prevState => ({ ...prevState, error: error?.error }));
+      } else {
+        setCommentsModalOptions(prevState => ({ ...prevState, error: 'Ocurrio un error inesperado' }));
+      }
+
+    } finally {
+      setCommentsModalOptions(prevState => ({ ...prevState, loading: false }));
     }
 
   };
@@ -87,9 +127,9 @@ const PostCard = ({
           <HiHeart className="likes" size="1.5em" />
           <span>{likes}</span>
         </span>
-        <span className="icon pointer">
+        <span onClick={handleOpenCommentsModal} className="icon pointer">
           <HiOutlineChatAlt2 size="1.5em" />
-          <span>15</span>
+          <span>Comentarios</span>
         </span>
       </div>
 
@@ -112,6 +152,32 @@ const PostCard = ({
         {(!userModalOptions.loading && !userModalOptions.error) && (
           <UserCard {...userModalOptions.data} />
         )}
+      </Modal>
+
+      <Modal
+        title="Comentarios"
+        onClose={handleCloseCommentsModal}
+        show={commentsModalOptions.show}
+        position="right"
+        transitionProps={{ onEntered: getPostComments }}
+      >
+        {commentsModalOptions.loading && <Loader />}
+
+        {commentsModalOptions.error && (
+          <p className="no-registers">
+            <span className="error-msg">{commentsModalOptions.error}</span>
+          </p>
+        )}
+
+        {(!commentsModalOptions.loading && !commentsModalOptions.error) && commentsModalOptions.comments.map(item => (
+          <CommentCard
+            key={item.id}
+            owner={item.owner}
+            message={item.message}
+            publishDate={item.publishDate}
+          />
+        ))}
+
       </Modal>
 
     </div>
