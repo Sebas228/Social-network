830c878dd4034b0e4ac9022d242628f7706d5063
Desarrollador Web <desarrolloweb@seguridadpenta.com>
2021-11-07 13:50:46 -0500

    Removidas credenciales de firebase y dummyapi

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -9,12 +9,12 @@ import {
 } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD985hh9V40kXC57q44SH-YTWKpAF0Sf9o",
-  authDomain: "socialnetworktest-9263f.firebaseapp.com",
-  projectId: "socialnetworktest-9263f",
-  storageBucket: "socialnetworktest-9263f.appspot.com",
-  messagingSenderId: "263040647736",
-  appId: "1:263040647736:web:db78b670795002a8755ccd"
+  apiKey: "////",
+  authDomain: "////",
+  projectId: "////",
+  storageBucket: "////",
+  messagingSenderId: "////",
+  appId: "////"
 };
 
 const app = initializeApp(firebaseConfig);
